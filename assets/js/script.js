9cb334bac4e343fd9a3e42e189411027931dd653
Valeria Rojas <valeriarojasmanriquez@gmail.com>
2020-08-21 17:18:23 -0400

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,15 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+
+
+$('a').click(function(){
+    alert("Click");
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 2000);
+    return false;
+});
+
+
+
